0da0c9a09944bfc69444bc9c14f960dad8a42428
benwestergreen <benwestergreen@gmail.com>
2023-01-30 14:22:33 -0800

    Fixed a Runtime error. Assignment ready to be submitted

--- Assets/Composer/NoteButton.cs
+++ Assets/Composer/NoteButton.cs
@@ -15,22 +15,18 @@ public class NoteButton : MonoBehaviour
     {
         // Get a reference to the Composer script
         composer = GameObject.Find("Composer").GetComponent<Composer>();
-        // Add the PlayNote method as a listener for the play button's onClick event
-        playButton.onClick.AddListener(PlayNote);
-        // Add the AddNoteToComposition method as a listener for the add button's onClick event
-        addButton.onClick.AddListener(AddNoteToComposition);
         // set remove as not active
         removeButton.gameObject.SetActive(false);
     }
 
     // Play the audio source attached to the prefab
-    private void PlayNote()
+    public void PlayNote()
     {
         audioSource.Play();
     }
 
     // Add the audio source attached to the prefab to the notesInComposition list
-    private void AddNoteToComposition()
+    public void AddNoteToComposition()
     {
         composer.AddNoteToComposition(audioSource);
     }
