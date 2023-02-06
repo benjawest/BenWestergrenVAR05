e6af0e2b6266ec873235421ccfab03360a30d9e3
benwestergreen <benwestergreen@gmail.com>
2023-02-05 17:03:29 -0800

    Backend for game functions in Console. Tile Instantiate works. Instantiate Pawns and moving pawns not working properly.

--- Assets/Race/boardGameManager.cs
+++ Assets/Race/boardGameManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
+using System.Threading;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class boardGameManager : MonoBehaviour
 {
     public int TileCount = 10;
-    public int playerIndex1;
-    public int playerIndex2;
+    public float TimeBetweenTurns = 0.2f;
+    public int playerIndex1 = 0;
+    public int playerIndex2 = 0;
+    public int TurnCount = 0;
     public List<string> boardTileTypes;
-    public int currentPlayerIndex;
+    public GameObject MudPrefab, GrassPrefab, FinishPrefab;
+    public GameObject Pawn1Prefab, Pawn2Prefab;
+    private Transform[] Pawn1Line, Pawn2Line;
+
 
 
 
@@ -17,7 +23,39 @@ public class boardGameManager : MonoBehaviour
     void Start()
     {
         GenerateBoardTileTypes(TileCount);
+        InstantiateTiles();
+        InstantiatePawns();
+
+        // Debug: Print content of board game tile types
+        Debug.Log("boardTileTypes: ");
+        foreach (var tile in boardTileTypes)
+        {
+            Debug.Log(tile);
+        }
+
+        StartCoroutine(Game());
     }
+
+    IEnumerator Game()
+    {
+        while (playerIndex1 < TileCount - 1 && playerIndex2 < TileCount - 1)
+        {
+            DidPawnMove(ref playerIndex1, "Player 1");
+            yield return new WaitForSeconds(TimeBetweenTurns);
+            DidPawnMove(ref playerIndex2, "Player 2");
+            yield return new WaitForSeconds(TimeBetweenTurns);
+            TurnCount++;
+        }
+        if (playerIndex1 >= TileCount - 1)
+        {
+            Debug.Log("Player 1 won after " + TurnCount + " turns!");
+        }
+        else if (playerIndex2 >= TileCount - 1)
+        {
+            Debug.Log("Player 2 won after " + TurnCount + " turns!");
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -25,11 +63,46 @@ public class boardGameManager : MonoBehaviour
         
     }
 
+    public void InstantiateTiles()
+    {
+        for (int i = 0; i < TileCount; i++)
+        {
+            GameObject newTile1;
+            GameObject newTile2;
 
+            if (boardTileTypes[i] == "mud")
+            {
+                newTile1 = Instantiate(MudPrefab, new Vector3(0, 0, i), Quaternion.identity);
+                newTile2 = Instantiate(MudPrefab, new Vector3(1, 0, i), Quaternion.identity);
+            }
+            else if (boardTileTypes[i] == "grass")
+            {
+                newTile1 = Instantiate(GrassPrefab, new Vector3(0, 0, i), Quaternion.identity);
+                newTile2 = Instantiate(GrassPrefab, new Vector3(1, 0, i), Quaternion.identity);
+            }
+            else
+            {
+                newTile1 = Instantiate(FinishPrefab, new Vector3(0, 0, i), Quaternion.identity);
+                newTile2 = Instantiate(FinishPrefab, new Vector3(1, 0, i), Quaternion.identity);
+            }
+        }
+    }
+
+    void InstantiatePawns()
+    {
+        Pawn1Line = new Transform[TileCount];
+        Pawn2Line = new Transform[TileCount];
+
+        Pawn1Line[0] = Instantiate(Pawn1Prefab, new Vector3(-0.5f, 0, 0), Quaternion.identity).transform;
+        Pawn2Line[0] = Instantiate(Pawn2Prefab, new Vector3(0.5f, 0, 0), Quaternion.identity).transform;
+    }
 
     // Generates tile types for the board game
-    void GenerateBoardTileTypes(int tileCount)
+    private void GenerateBoardTileTypes(int tileCount)
     {
+        // Clear board
+        boardTileTypes.Clear();
+
         //Generate the list of tile types
         boardTileTypes = new List<string>();
         for (int i = 0; i < tileCount - 1; i++)
@@ -46,15 +119,32 @@ public class boardGameManager : MonoBehaviour
             }
         }
         boardTileTypes.Add("finish");
+    }
 
-        // Debug: Print the content of boardTileTypes
-        //Print the contents of boardTileTypes
-        foreach (string tileType in boardTileTypes)
+    // Checks player moves ahead based on current tile type
+    void DidPawnMove(ref int playerIndex, string playerName)
+    {
+        Debug.Log("Turn: " + TurnCount + " " + playerName + " on tile: " + playerIndex);
+        if (boardTileTypes[playerIndex] == "grass")
         {
-            Debug.Log(tileType);
+            System.Random rand = new System.Random();
+            int randomInt = rand.Next(0, 4);
+            if (randomInt == 0)
+            {
+                playerIndex++;
+                Debug.Log(playerName + " moved to tile: " + playerIndex);
+            }
+        }
+        else if (boardTileTypes[playerIndex] == "mud")
+        {
+            System.Random rand = new System.Random();
+            int randomInt = rand.Next(0, 8);
+            if (randomInt == 0)
+            {
+                playerIndex++;
+                Debug.Log(playerName + " moved to tile: " + playerIndex);
+            }
         }
     }
 
-
-  
 }
